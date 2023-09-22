8e16f1b4a626aa4744625cf2dcc16d9255b63119
jackyzhong <jackyecnu@126.com>
2023-09-23 07:20:46 +0800

    增加basecontroller

---
+++ src/main/java/com/lm/springbootstandardproject/controllers/BaseController.java
@@ -0,0 +1,37 @@
+package com.lm.springbootstandardproject.controllers;
+
+import com.lm.tools.DemonConstants;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public abstract class BaseController {
+
+
+    @Autowired
+    protected HttpServletRequest request;
+
+    /**
+     * 获取请求中的token
+     *
+     */
+    protected String getToken() {
+        return request.getHeader("token") == null ? "" : request.getHeader("token").toString();
+    }
+
+
+    /**
+     * 获取请求中的userId
+     */
+    protected String getUserId() {
+        return request.getAttribute(DemonConstants.Log.SenderId) == null ? "" : request.getAttribute(DemonConstants.Log.SenderId).toString();
+    }
+
+    /*
+     * 获取请求中的userAgent
+     * */
+    protected String getUserUserAgent() {
+        return request.getHeader("User-Agent");
+    }
+
+}
