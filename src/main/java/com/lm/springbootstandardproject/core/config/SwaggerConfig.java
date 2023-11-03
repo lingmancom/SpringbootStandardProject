0c0694c14e36256997bf77e609fc2fa1c215574b
Jacky 钟双金 <jackyecnu@126.com>
2023-11-03 21:35:22 +0800

    接口文档

--- src/main/java/com/lm/springbootstandardproject/core/config/SwaggerConfig.java
+++ src/main/java/com/lm/springbootstandardproject/core/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.lm.springbootstandardproject.core.config;
 
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
@@ -10,37 +9,31 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class SwaggerConfig {
-    private static final String headerName = "Token";//请求头名称
+        private static final String headerName = "Token";// 请求头名称
 
-    @Bean
-    public OpenAPI openApi() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("曼特韦德实训平台接口")
-                        .description("曼特韦德实训平台接口")
-                        .contact(new Contact().name("lingman").email("邮箱").url("公司官网地址"))
-                        .version("v1.0")
-                )
-                .addSecurityItem(new SecurityRequirement().addList(headerName))
-                .components(components()
-                );
-    }
+        @Bean
+        public OpenAPI openApi() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("接口文档")
+                                                .description("接口文档")
+                                                .contact(new Contact().name("lingman").email("邮箱").url("公司官网地址"))
+                                                .version("v1.0"))
+                                .addSecurityItem(new SecurityRequirement().addList(headerName))
+                                .components(components());
+        }
 
-    private Components components() {
-        return new Components()
-                .addSecuritySchemes(
-                        headerName,
-                        new SecurityScheme()
-                                .type(SecurityScheme.Type.APIKEY)
-                                .scheme("basic")
-                                .name(headerName)
-                                .in(SecurityScheme.In.HEADER)
-                                .description("token令牌")
-                )
-                ;
-    }
+        private Components components() {
+                return new Components()
+                                .addSecuritySchemes(
+                                                headerName,
+                                                new SecurityScheme()
+                                                                .type(SecurityScheme.Type.APIKEY)
+                                                                .scheme("basic")
+                                                                .name(headerName)
+                                                                .in(SecurityScheme.In.HEADER)
+                                                                .description("token令牌"));
+        }
 }
-
