af23ffe513589f3e0cdb538e930b38f9c1086ae9
Jacky 钟双金 <jackyecnu@126.com>
2023-11-21 09:28:51 +0800

    发版: release

--- src/main/java/com/lm/springbootstandardproject/core/config/MybatisPlusConfig.java
+++ src/main/java/com/lm/springbootstandardproject/core/config/MybatisPlusConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.lingman.work.dal.mapper")
 public class MybatisPlusConfig {
 
     /**
